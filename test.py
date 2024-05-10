21bf8a37857e32a01001802e136265967804c2ab
Bafbi <bafbi3@gmail.com>
2024-05-11 00:06:11 +0200

    still working on codec

--- test.py
+++ test.py
@@ -5,22 +5,40 @@ import getopt
 import sys
 import json
 
-def check_simple_write(instructions: List[Any]):
+def check_simple_encode(instructions: List[Any]):
+    '''
+    [0000]│   ├─<Instruction('getfield')>
+    [0000]│   │ └─<FieldReference('net/minecraft/network/protocol/game/ClientboundContainerSetContentPacket', 'containerId', 'I')>
+    [0000]│   ├─<Instruction('invokevirtual')>
+    [0000]│   │ └─<MethodReference('net/minecraft/network/RegistryFriendlyByteBuf', 'writeByte', '(I)Lnet/minecraft/network/FriendlyByteBuf;')>
+    '''
     assert len(instructions) >= 2
-    field = []
-    type_ = None
-    assert instructions[0].name == "getfield"
-    field.append(instructions[0].find_one(name="FieldReference").target)
-    if instructions[1].name == "invokeinterface":
-        field.append(instructions[1].find_one(name="InterfaceMethodRef").target)
-        assert instructions[2].name == "invokevirtual"
-        type_ = instructions[2].find_one(name="MethodReference").target
-    else:
-        assert instructions[1].name == "invokevirtual"
-        type_ = instructions[1].find_one(name="MethodReference").target
-    return ".".join(field), type_
+    field = None
+    assert instructions[1].name == "invokevirtual"
+    instruction_method_ref = instructions[1].find_one(name="MethodReference")
+    assert instruction_method_ref.class_ == "net/minecraft/network/RegistryFriendlyByteBuf"
+    type_ = instruction_method_ref.target
+    if instructions[0].name == "getfield":
+        field = instructions[0].find_one(name="FieldReference").target
+    elif instructions[0].name == "invokevirtual":
+        method_ref = instructions[0].find_one(name="MethodReference")
+        field = f"{method_ref.class_}.{method_ref.target}"
+    elif instructions[0].name == "iconst_0":
+        field = "iconst_0"
 
-def check_object_write(instructions: List[Any]):
+    # assert instructions[0].name == "getfield"
+    # field.append(instructions[0].find_one(name="FieldReference").target)
+    # if instructions[1].name == "invokeinterface":
+    #     field.append(instructions[1].find_one(name="InterfaceMethodRef").target)
+    #     assert instructions[2].name == "invokevirtual"
+    #     type_ = instructions[2].find_one(name="MethodReference").target
+    # else:
+    #     assert instructions[1].name == "invokevirtual"
+    #     type_ = instructions[1].find_one(name="MethodReference").target
+    # return ".".join(field), type_
+    return field, type_
+
+def check_object_encode(instructions: List[Any]):
     '''
     [0000]│   ├─<Instruction('getfield')>
     [0000]│   │ └─<FieldReference('net/minecraft/network/protocol/login/ClientboundCustomQueryPacket', 'payload', 'Lnet/minecraft/network/protocol/login/custom/CustomQueryPayload;')>
@@ -50,6 +68,26 @@ def check_simple_codec(instructions: List[Any], codec_name: str):
     assert instructions[1].find_one(name="FieldReference").target == codec_name
     return instructions[0].find_one(name="InterfaceMethodRef").target
 
+def check_complex_codec(instructions: List[Any], codec_name: str, loader: ClassLoader):
+    '''
+    We first find the method class then load it and got read the 'encode' method
+    [0000]│   ├─<Instruction('invokespecial')>
+    [0000]│   │ └─<MethodReference('net/minecraft/world/item/ItemStack$1', '<init>', '()V')>
+    [0000]│   ├─<Instruction('putstatic')>
+    [0000]│   │ └─<FieldReference('net/minecraft/world/item/ItemStack', 'OPTIONAL_STREAM_CODEC', 'Lnet/minecraft/network/codec/StreamCodec;')>
+    '''
+    assert len(instructions) >= 2
+    assert instructions[0].name == "invokespecial"
+    assert instructions[1].name == "putstatic"
+    assert instructions[1].find_one(name="FieldReference").target == codec_name
+    class_ = instructions[0].find_one(name="MethodReference").class_
+    ccf = loader[class_]
+    for method in ccf.methods.find():
+        print(method)
+    encode_method = ccf.methods.find_one(name = "encode", returns = "V")
+    print(encode_method.pretty())
+    return method_scanner(encode_method, loader)
+
 
 def check_codec_write(instructions: List[Any], loader: ClassLoader):
     '''
@@ -72,9 +110,9 @@ def check_codec_write(instructions: List[Any], loader: ClassLoader):
     assert instructions[4].name == "invokeinterface"
     codec = instructions[0].find_one(name="FieldReference")
     ccf = loader[codec.class_]
-    print(ccf.methods.find_one(name = "<clinit>", returns = "V").pretty())
+    # print(ccf.methods.find_one(name = "<clinit>", returns = "V").pretty())
     codec_instruction = list(ccf.methods.find_one(name = "<clinit>", returns = "V").code.find(name= "Instruction"))
-    print(codec_instruction)
+    # print(codec_instruction)
     codec_name = instructions[0].find_one(name="FieldReference").target
     type_ = None
     while len(codec_instruction) > 0:
@@ -82,10 +120,35 @@ def check_codec_write(instructions: List[Any], loader: ClassLoader):
             type_ = check_simple_codec(codec_instruction, codec_name)
         except AssertionError:
             pass
+        try:
+            type_ = check_complex_codec(codec_instruction, codec_name, loader)
+        except AssertionError:
+            pass
         codec_instruction = codec_instruction[1:]
     return field, type_
 
-
+def method_scanner(method: ast.Method, loader: ClassLoader) -> Dict[str, Any]:
+    print(method.pretty())
+    instructions = list(method.code.find(name="Instruction"))
+    writes: Dict[str, Any] = {}
+    while len(instructions) > 0:
+        try:
+            field = check_simple_encode(instructions)
+            writes[field[0]] = field[1]
+        except AssertionError:
+            pass
+        try:
+            field = check_object_encode(instructions)
+            writes[field[0]] = field[1]
+        except AssertionError:
+            pass
+        try:
+            field = check_codec_write(instructions, loader)
+            writes[field[0]] = field[1]
+        except AssertionError:
+            pass
+        instructions = instructions[1:]
+    return writes
 
 
 if __name__ == '__main__':
@@ -113,55 +176,13 @@ if __name__ == '__main__':
     # cf = loader["net/minecraft/network/protocol/common/ClientboundStoreCookiePacket"]
     cf = loader["net/minecraft/network/protocol/game/ClientboundContainerSetContentPacket"]
 
-    print(cf)
     is_record = cf.attributes.find_one(type_ = ast.UnknownAttribute, f=lambda x: x.name == "Record") is not None
-
-    stream_codec_field = cf.fields.find_one(name = "STREAM_CODEC")
-    print(stream_codec_field.pretty())
-
-    stream_codec_method = cf.methods.find_one(name = "<clinit>", returns = "V")
-    print(stream_codec_method.pretty())
-
-
     use_write = cf.methods.find_one(name = "write", returns = "V") is not None
-
-    # # Idiomatic way to check if this chain of calls returns a non-None value
-    # try:
-    #     use_write = cf.methods.find_one(name = "<clinit>", returns = "V").code.find_one(name = "Instruction", f = lambda x: x.name == "invokestatic").find_one(name = "InterfaceMethodRef", f = lambda x: x.class_ == "net/minecraft/network/protocol/Packet" and x.target == "codec") is not None
-    # except AttributeError:
-    #     pass
     print(f"Is record: {is_record}, use write: {use_write}")
-
-    # [0000]│   ├─<Instruction('invokeinterface')>
-    # [0000]│   │ ├─<InterfaceMethodRef('net/minecraft/network/codec/StreamCodec', 'map', '(Ljava/util/function/Function;Ljava/util/function/Function;)Lnet/minecraft/network/codec/StreamCodec;')>
-
-
-    for method in cf.methods.find():
-        print(method)
 
     method = cf.methods.find_one(name = "write", returns = "V")
     # print(method.pretty())
 
-    instructions = list(method.code.find(name = "Instruction"))
-    # print(instructions)
+    structure = method_scanner(method, loader)
 
-    writes = []
-    while len(instructions) > 0:
-        try:
-            a = check_simple_write(instructions)
-            writes.append(a)
-        except AssertionError:
-            pass
-        try:
-            a = check_object_write(instructions)
-            writes.append(a)
-        except AssertionError:
-            pass
-        try:
-            a = check_codec_write(instructions, loader)
-            writes.append(a)
-        except AssertionError:
-            pass
-        instructions = instructions[1:]
-
-    print(writes)
+    print(structure)
